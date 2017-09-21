fd9ec776a28d34562fd22ff3a4a4987dd6391436
SHINYA <shinya.s.jpn@gmail.com>
2017-09-21 11:18:21 +0900

    Update 5_recovery.sh

--- 5_recovery.sh
+++ 5_recovery.sh
@@ -3,7 +3,7 @@
 mysqlsh << EOL
 
 dba.startSandboxInstance(3310);
-shell.connect ('root:password@localhost:3310')
+shell.connect ('root:password@localhost:3320')
 var cluster = dba.getCluster("singleCluster")
 cluster.rejoinInstance('root:password@localhost:3310')
 cluster.status()
